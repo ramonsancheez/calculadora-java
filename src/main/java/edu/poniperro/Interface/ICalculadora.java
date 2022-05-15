618da943fd45065a57a2dccc2a2a1877d1d3dce8
Ramon <rsanchezabadia@cifpfbmoll.eu>
2022-05-15 12:47:01 +0200

    refactor: delete public visibility

--- src/main/java/edu/poniperro/Interface/ICalculadora.java
+++ src/main/java/edu/poniperro/Interface/ICalculadora.java
@@ -1,8 +1,8 @@
 package edu.poniperro.Interface;
 
 public interface ICalculadora {
-    public double sumar(double num1, double num2);
-    public double restar(double num1,double num2);
-    public double multiplicar(double num1, double num2);
-    public double dividir(double num1, double num2);
+    double sumar(double num1, double num2);
+    double restar(double num1,double num2);
+    double multiplicar(double num1, double num2);
+    double dividir(double num1, double num2);
 }
